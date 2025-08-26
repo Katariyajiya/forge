aaa2acce2a6c28ae651c4281813a76b0842d5ceb
Jiya Katariya <149702427+Katariyajiya@users.noreply.github.com>
2025-08-26 23:52:52 +0530

    Add files via upload

---
+++ lib/pages/chef_detail.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ChefDetail extends StatefulWidget {
+  const ChefDetail({super.key});
+
+  @override
+  State<ChefDetail> createState() => _ChefDetailState();
+}
+
+class _ChefDetailState extends State<ChefDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
