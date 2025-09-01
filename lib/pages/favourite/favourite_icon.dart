1d89b21d97d82d2abd4b4989f3512b6f5efe8d2d
Jiya Katariya <149702427+Katariyajiya@users.noreply.github.com>
2025-09-02 00:45:46 +0530

    Create favourite_icon.dart

---
+++ lib/pages/favourite/favourite_icon.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FavouriteIcon extends StatelessWidget{
+
+  const FavouriteIcon(
+  {
+    super.key
+  });
+
+  @override
+  Widget build(BuildContext context){
+    return Icon( Icons.favorite_border_outlined,
+      size: 20,
+      color: Colors.white,
+    );
+
+
+  }
+
+
+
+
+
+}
+
+
